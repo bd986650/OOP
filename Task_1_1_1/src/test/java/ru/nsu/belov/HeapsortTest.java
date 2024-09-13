4d537c144564032d14d68a1a3713e739f5301af6
qwotic <qwoticreator@gmail.com>
2024-09-13 14:06:22 +0700

    testing

--- Task_1_1_1/src/test/java/ru/nsu/belov/HeapsortTest.java
+++ Task_1_1_1/src/test/java/ru/nsu/belov/HeapsortTest.java
@@ -1,12 +1,8 @@
 package ru.nsu.belov;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class HeapsortTest {
-
-    @Test
     public void testSort() {
         int[] arr = {-1, 1, 0, 5, -50, 0};
         int[] res = {-50, -1, 0, 0, 1, 5};
@@ -15,7 +11,6 @@ class HeapsortTest {
         assertEquals(res, arr);
     }
 
-    @Test
     public void testSortPositive() {
         int[] arr = {10, 9, 8, 4, 5, 2};
         int[] res = {2, 4, 5, 8, 9, 10};
@@ -24,7 +19,6 @@ class HeapsortTest {
         assertEquals(res, arr);
     }
 
-    @Test
     public void testSortNegative() {
         int[] arr = {-1 , -12, -10, -5, -2};
         int[] res = {-12, -10, -5, -2, -1};
@@ -33,7 +27,6 @@ class HeapsortTest {
         assertEquals(res, arr);
     }
 
-    @Test
     public  void testSortEmpty() {
         int[] arr = {};
         int[] res = {};
