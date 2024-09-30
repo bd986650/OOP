3ac8943b19f941ddd2b0e72d722aafa0bad32774
bd986650 <96487423+bd986650@users.noreply.github.com>
2024-09-30 12:20:15 +0700

    Update DeckTest.java

--- Task_1_1_2/src/test/java/ru/nsu/belov/DeckTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/belov/DeckTest.java
@@ -10,18 +10,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class DeckTest {
     private ArrayList<Card> new_cards_for_test = new ArrayList<>();
 
-//    @Test
-//    void create_deck_test() {
-//        Deck deck = new Deck();
-//        for (int rank = 0; rank < 4; rank++) {
-//            for (int suit = 0; suit < 13; suit++) {
-//                new_cards_for_test.add(new Card(suit, rank));
-//            }
-//        }
-//        Collections.shuffle(new_cards_for_test);
-//        assertEquals(new_cards_for_test, deck.cards);
-//    }
-
     @Test
     void shuffle_test() {
         ArrayList<Card> new_cards_for_test = new ArrayList<>();
