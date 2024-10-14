029f02679867e08c713cf4926a77872bd78736ba
qwotic <qwoticreator@gmail.com>
2024-10-14 13:11:36 +0700

    checkstyle check

--- Task_1_1_2/src/test/java/ru/nsu/belov/DealerTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/belov/DealerTest.java
@@ -1,11 +1,11 @@
 package ru.nsu.belov;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
