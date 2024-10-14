a8152b97014a4be171fd6784843d44f62d1cfdc6
qwotic <qwoticreator@gmail.com>
2024-10-14 13:30:10 +0700

    checkstyle check

--- Task_1_1_2/src/main/java/ru/nsu/belov/Player.java
+++ Task_1_1_2/src/main/java/ru/nsu/belov/Player.java
@@ -77,28 +77,3 @@ public class Player {
     }
 }
 
-/**
- * dealer class from player class.
- */
-class Dealer extends Player {
-    /**
-     * dealer.
-     */
-    public Dealer() {
-        super("Dealer");
-    }
-
-    /**
-     * show start hand.
-     */
-    public void showStartHand() {
-        System.out.println("    Dealer's cards: " + "[" + hand.get(0) + ", <closed card>" + "]");
-    }
-
-    /**
-     * print hand for dealer.
-     */
-    public void printHand() {
-        System.out.println("    Dealer's cards: " + hand + " => " + getPointForPlayer());
-    }
-}
