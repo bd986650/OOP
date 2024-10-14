21b22ca9b6d62616e4e281a0d1ceadc9ba017529
qwotic <qwoticreator@gmail.com>
2024-10-14 13:08:06 +0700

    checkstyle check

--- Task_1_1_2/src/test/java/ru/nsu/belov/PlayerTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/belov/PlayerTest.java
@@ -1,11 +1,11 @@
 package ru.nsu.belov;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 
 /**
  * Player Test.
