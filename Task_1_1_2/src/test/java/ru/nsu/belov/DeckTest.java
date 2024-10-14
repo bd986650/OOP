d1db90052bd411cd4745a7ede353f9f7fed1807a
qwotic <qwoticreator@gmail.com>
2024-10-14 13:05:31 +0700

    checkstyle check

--- Task_1_1_2/src/test/java/ru/nsu/belov/DeckTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/belov/DeckTest.java
@@ -1,11 +1,11 @@
 package ru.nsu.belov;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.util.HashSet;
 import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Deck Test.
