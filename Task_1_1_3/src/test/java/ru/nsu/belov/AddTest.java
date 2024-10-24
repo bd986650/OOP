1e0476fd65320c4c45351aa042664ca7826ef630
Danil Belov <d.belov2@g.nsu.ru>
2024-10-24 10:14:51 +0700

    Task_1_1_3

--- Task_1_1_3/src/test/java/ru/nsu/belov/AddTest.java
+++ Task_1_1_3/src/test/java/ru/nsu/belov/AddTest.java
@@ -1,9 +1,8 @@
 package ru.nsu.belov;
 
-import org.junit.jupiter.api.Test;
-
 import java.util.HashMap;
 import java.util.Map;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
--- Task_1_1_3/src/test/java/ru/nsu/belov/NumberTest.java
+++ Task_1_1_3/src/test/java/ru/nsu/belov/NumberTest.java
@@ -1,8 +1,7 @@
 package ru.nsu.belov;
 
-import org.junit.jupiter.api.Test;
-
 import java.util.HashMap;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
