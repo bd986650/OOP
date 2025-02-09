cf0bc84db0f36dfba3c53b82bb39952ae55848e3
Danil Belov <d.belov2@g.nsu.ru>
2025-02-09 21:56:30 +0700

    Task_2_1_1

--- Task_2_1_1/src/test/java/ru/nsu/belov/FindPrimeTest.java
+++ Task_2_1_1/src/test/java/ru/nsu/belov/FindPrimeTest.java
@@ -7,11 +7,8 @@ import static org.junit.jupiter.api.Assertions.*;
 class FindPrimeTest {
     @Test
     void isPrimeTest() {
-        assertTrue(FindPrime.isPrime(0));
-        assertTrue(FindPrime.isPrime(1));
+        assertTrue(FindPrime.isPrime(7));
+        assertFalse(FindPrime.isPrime(10));
         assertTrue(FindPrime.isPrime(2));
-        assertTrue(FindPrime.isPrime(3));
-        assertTrue(FindPrime.isPrime(4));
-        assertTrue(FindPrime.isPrime(10));
     }
 }
