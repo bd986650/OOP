09a0de3db99ef0db40b774bee2bf9e52779a5e1a
bd986650 <96487423+bd986650@users.noreply.github.com>
2025-02-23 16:09:07 +0700

    Update ParallelThread.java

--- Task_2_1_1/src/main/java/ru/nsu/belov/ParallelThread.java
+++ Task_2_1_1/src/main/java/ru/nsu/belov/ParallelThread.java
@@ -7,25 +7,30 @@ import java.util.concurrent.atomic.AtomicBoolean;
 /**
  * Parallel thread class.
  */
-class ParallelThread {
+public class ParallelThread {
+    private static volatile boolean foundNotPrime = false;
+
     public static boolean hasNonPrimeParallelThreads(int[] numbers, int threadCount)
             throws InterruptedException {
         int chunkSize = (int) Math.ceil(numbers.length / (double) threadCount);
-        List<Thread> threads = new CopyOnWriteArrayList<>();
-        AtomicBoolean foundNotPrime = new AtomicBoolean(false);
+
+        List<Thread> threads = Collections.synchronizedList(new ArrayList<>());
+
+        foundNotPrime = false;
 
         for (int i = 0; i < threadCount; i++) {
             int start = i * chunkSize;
             int end = Math.min(start + chunkSize, numbers.length);
 
             Thread thread = new Thread(() -> {
-                for (int j = start; j < end && !foundNotPrime.get(); j++) {
-                    if (!FindPrime.isPrime(numbers[j])) {
-                        foundNotPrime.set(true);
+                for (int j = start; j < end && !foundNotPrime; j++) {
+                    if (!PrimeUtil.isPrime(numbers[j])) {
+                        foundNotPrime = true;
                         break;
                     }
                 }
             });
+
             threads.add(thread);
             thread.start();
         }
@@ -34,6 +39,6 @@ class ParallelThread {
             thread.join();
         }
 
-        return foundNotPrime.get();
+        return foundNotPrime;
     }
 }
