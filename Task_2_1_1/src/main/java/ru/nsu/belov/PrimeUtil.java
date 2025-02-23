f186e5b14fdac57391bc581165deea98c8bad965
Danil Belov <d.belov2@g.nsu.ru>
2025-02-23 16:19:09 +0700

    checkstyle upd

--- Task_2_1_1/src/main/java/ru/nsu/belov/PrimeUtil.java
+++ Task_2_1_1/src/main/java/ru/nsu/belov/PrimeUtil.java
@@ -4,6 +4,12 @@ package ru.nsu.belov;
  * Find prime number class.
  */
 public class PrimeUtil {
+    /**
+     * main function of PrimeUtil.
+     *
+     * @param num main input
+     * @return boolean value
+     */
     public static boolean isPrime(int num) {
         if (num < 2) {
             return false;
@@ -16,6 +22,9 @@ public class PrimeUtil {
         return true;
     }
 
+    /**
+     * private constructor for git push review.
+     */
     private PrimeUtil() {
         throw new UnsupportedOperationException("This is a utility class and cannot be instantiated");
     }
