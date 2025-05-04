5716e88a4bd07f65f5482a31855eae3c3fb03aea
Danil Belov <d.belov2@g.nsu.ru>
2025-05-04 14:28:53 +0700

    add new controller

--- Task_2_3_1/src/main/java/ru/nsu/belov/controller/GameEndController.java
+++ Task_2_3_1/src/main/java/ru/nsu/belov/controller/GameEndController.java
@@ -1,56 +1,22 @@
 package ru.nsu.belov.controller;
 
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
-import javafx.util.Duration;
 import ru.nsu.belov.Main;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class GameEndController {
     @FXML private Label resultLabel;
     @FXML private HBox appleContainer;
+    @FXML private HBox topAppleContainer;
 
-    private final List<ImageView> apples = new ArrayList<>();
-    private Timeline timeline;
+    private AppleAnimationController bottomAppleAnimation;
+    private AppleAnimationController topAppleAnimation;
 
     @FXML
     private void initialize() {
-        Image appleImage = new Image(getClass().getResourceAsStream("/apple.png"));
-        for (int i = 0; i < 11; i++) {
-            ImageView apple = new ImageView(appleImage);
-            apple.setFitHeight(50);
-            apple.setPreserveRatio(true);
-            apples.add(apple);
-            appleContainer.getChildren().add(apple);
-        }
-
-        startAppleAnimation();
-    }
-
-    private void startAppleAnimation() {
-        timeline = new Timeline(new KeyFrame(Duration.millis(30), event -> moveApples()));
-        timeline.setCycleCount(Timeline.INDEFINITE);
-        timeline.play();
-    }
-
-    private void moveApples() {
-        double speed = 2;
-        double sceneWidth = appleContainer.getScene().getWidth();
-
-        for (ImageView apple : apples) {
-            apple.setTranslateX(apple.getTranslateX() + speed);
-
-            if (apple.localToScene(apple.getBoundsInLocal()).getMinX() > sceneWidth) {
-                apple.setTranslateX(apple.getTranslateX() - sceneWidth - 100);
-            }
-        }
+        bottomAppleAnimation = new AppleAnimationController(appleContainer);
+        topAppleAnimation = new AppleAnimationController(topAppleContainer);
     }
 
     public void setResult(String result) {
