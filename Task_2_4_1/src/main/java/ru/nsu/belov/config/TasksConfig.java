cfb803b236a662fdb7289f56bdcdbbf0ede5ebc3
Danil Belov <d.belov2@g.nsu.ru>
2025-05-05 07:20:15 +0700

    upd added new tasks

--- Task_2_4_1/src/main/java/ru/nsu/belov/config/TasksConfig.java
+++ Task_2_4_1/src/main/java/ru/nsu/belov/config/TasksConfig.java
@@ -15,7 +15,12 @@ public class TasksConfig {
             "Task_1_2_2",
             "Task_1_3_1",
             "Task_1_4_1",
-            "Task_1_5_1"
+            "Task_1_5_1",
+            "Task_2_1_1",
+            "Task_2_1_2",
+            "Task_2_2_1",
+            "Task_2_3_1",
+            "Task_2_4_1"
         );
     }
 
